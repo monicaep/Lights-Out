d6c44f346c9d1247aa3acb21d35550b8308f32ec
monicaep <mep823@gmail.com>
2020-05-15 16:11:42 -0700

    Updates

---
+++ precache-manifest.f415f4477e3183d1a57a504ccee386b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479df60ed5a13391edaef8401d6a7a2f",
+    "url": "/Lights-Out/index.html"
+  },
+  {
+    "revision": "6b1d68df34bf2f6f9375",
+    "url": "/Lights-Out/static/css/main.a3d7d721.chunk.css"
+  },
+  {
+    "revision": "84bc0dedf56610160276",
+    "url": "/Lights-Out/static/js/2.6ff70722.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Lights-Out/static/js/2.6ff70722.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b1d68df34bf2f6f9375",
+    "url": "/Lights-Out/static/js/main.5f462534.chunk.js"
+  },
+  {
+    "revision": "deea540a38ef4f581a8a",
+    "url": "/Lights-Out/static/js/runtime-main.ba8c0907.js"
+  }
+]);
